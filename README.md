3bc3296c6fb90c5b7d57cffa06ec2e557c502432
wubao <wubao@qudian.com>
2024-03-05 18:04:55 +0800

    feat: add hook

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
+```
+# mq-go2sky-hook
 
+用于[work](https://github.com/qit-team/work) 包消息入队的hook
+
+
+## Example
+```go
+package main
+
+import (
+    "log"
+    "context"
+    "time"
+    
+    ""github.com/qit-team/work"  // version>=0.3.12
+
+    mqSkyHook "github.com/qit-team/mq-go2sky-hook"
+	"github.com/SkyAPM/go2sky"
+)
+
+
+func main() {
+	tracer, err := go2sky.NewTracer("127.0.0.1:11800")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var ctx = context.Background()
+	hook := mqSkyHook.NewHook(tracer)
+	job := work.New()
+	job.AddHook(hook)
+	
+	// do an equeue test for job
+	...
+}
+```
+
+```
+
+```
---
+++ plugin.go
@@ -0,0 +1,56 @@
+package mq_go2sky_plugin
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/SkyAPM/go2sky"
+	v3 "github.com/SkyAPM/go2sky/reporter/grpc/language-agent"
+	"github.com/qit-team/work"
+)
+
+type Hook struct {
+	tracer *go2sky.Tracer
+}
+
+func NewHook(tracer *go2sky.Tracer) *Hook {
+	return &Hook{tracer: tracer}
+}
+
+func (h *Hook) BeforeProcess(c *work.ContextHook) error {
+	peer := "queue"
+	if p, ok := c.Ctx.Value("peer").(string); ok {
+		peer = p
+	}
+
+	args := fmt.Sprintf("%v", c.Args)
+	operateName := fmt.Sprintf("enqueue_%v", c.Topic)
+	span, nCtx, err := h.tracer.CreateLocalSpan(c.Ctx, go2sky.WithSpanType(go2sky.SpanTypeLocal), go2sky.WithOperationName(operateName))
+	if err != nil {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	span.SetPeer(peer)
+	span.Tag("args", args)
+	span.Tag("topic", c.Topic)
+	span.SetSpanLayer(v3.SpanLayer_MQ)
+	c.Ctx = nCtx
+	c.Ctx = context.WithValue(c.Ctx, operateName, span)
+	return nil
+}
+
+func (h *Hook) AfterProcess(c *work.ContextHook) error {
+	span := c.Ctx.Value(fmt.Sprintf("enqueue_%v", c.Topic)).(go2sky.Span)
+	if c.ExecuteTime > 0 {
+		span.Tag("elapsed", fmt.Sprintf("%d ms", c.ExecuteTime.Milliseconds()))
+	}
+	if c.Err != nil {
+		timeNow := time.Now()
+		span.Error(timeNow, c.Err.Error())
+	}
+	span.End()
+	return nil
+}
